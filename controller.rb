424f2fbfbae450d8b8f80f8ea3bdc58fe17891d4
Daniel Childs <dc.1994@outlook.com>
2018-04-18 14:26:48 +0100

    create edit metohd and edit for edit.erb

--- controller.rb
+++ controller.rb
@@ -11,15 +11,28 @@ get "/hogwarts" do
 end
 
 #new
-get "/hogwarts/new" do 
+get "/hogwarts/new" do
   erb(:new)
 end
 
 #show
+get "/hogwarts/:id" do
+  @student = Student.find_by_id(params[:id])
+  erb(:show)
+end
 
 #create
+post "/hogwarts" do
+  student = Student.new(params)
+  student.save()
+  redirect to "/hogwarts"
+end
 
 #edit
+get "/hogwarts/:id/edit" do
+ @student = Student.find_by_id(params[:id])
+ erb(:edit)
+end
 
 #update
 
